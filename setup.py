471950b39ca94ece16cac06021edc58267916f45
Diego Lopez <dlopez@comparaonline.com>
2020-01-24 12:21:05 -0300

    fix setup

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='testlibrary',
-    version='0.1',
+    version='0.1.1',
     description='This is a test for a tutorial on how to create a python library',  # noqa
     url='git@github.com:dlopezco/test-lib.git',
-    download_url='https://github.com/dlopezco/test-library/archive/v0.1.tar.gz',  # noqa
+    download_url='https://v0.1@/archive/v0.1.1.tar.gz',  # noqa
     author='Diego',
     author_email='dlopez@comparaonline.com',
     license='MIT',
