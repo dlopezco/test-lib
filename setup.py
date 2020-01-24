125b85e04db666c745d0ef047db6d15eaa5bd4f3
Diego Lopez <dlopez@comparaonline.com>
2020-01-24 11:52:38 -0300

    replace urls

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ setup(
     name='testlibrary',
     version='0.1',
     description='This is a test for a tutorial on how to create a python library',  # noqa
-    # url='git@github.com:dlopezco/test-lib.git',
-    download_url='https://github.com/comparaonline/etl-builder/archive/v1.3.4.tar.gz',  # noqa
-    author='Compara',
-    author_email='bi@comparaonline.com',
+    url='git@github.com:dlopezco/test-lib.git',
+    download_url='https://github.com/dlopezco/test-library/archive/v0.1.tar.gz',  # noqa
+    author='Diego',
+    author_email='dlopez@comparaonline.com',
     license='MIT',
     packages=find_packages(),
     zip_safe=False,
